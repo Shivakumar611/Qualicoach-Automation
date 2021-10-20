44d0d34db4e2226e16004946e6b3146c97ea6ded
Shivakumar611 <shivuun9@gmail.com>
2021-10-20 22:58:55 +0530

    testng push

---
+++ Mavenjava/src/test/java/students/AaUserFunctiontest.java
@@ -0,0 +1,28 @@
+package students;
+
+import java.io.IOException;
+
+public class AaUserFunctiontest {
+  //Generalized object creation
+	 
+	attemptQuiz a1 = new attemptQuiz();
+	 courseenroll a2 = new courseenroll();
+	 feedbackTest a3 = new feedbackTest();
+	 stu_profileTest a4 = new stu_profileTest();
+	 view_down_certificate a5 = new view_down_certificate();
+
+	public void UserTest() throws IOException {
+	
+		//Generalized function calls
+		a1.Quiz();
+		
+		a2.enrollTest();
+		
+		a3.feedback();
+		
+		a4.studentProfile();
+		
+		a5.cert();
+		
+	}
+}
